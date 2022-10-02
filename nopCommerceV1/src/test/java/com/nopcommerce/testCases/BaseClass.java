111bc266d21d1f495bc6bb04fae580dc6f3acedb
Amir Ansari <amir143in@gmail.com>
2022-10-01 20:59:33 -0500

    second changes

--- nopCommerceV1/src/test/java/com/nopcommerce/testCases/BaseClass.java
+++ nopCommerceV1/src/test/java/com/nopcommerce/testCases/BaseClass.java
@@ -16,7 +16,6 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
